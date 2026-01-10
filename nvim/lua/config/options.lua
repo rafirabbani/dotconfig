4f56a5681f8cfb7aa5a4c841a2b07d962c3a1028
Yusuf Rafi Rabbani <rafirabbani103@gmail.com>
2026-01-11 03:07:57 +0700

    Add async function pipe copied buffer to windows clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,12 +1,40 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local function async_copy(lines)
+  local stdin = vim.uv.new_pipe()
+  local stderr = vim.uv.new_pipe()
+  vim.uv.spawn("win32yank.exe", {
+    args = { "-i", "--crlf" },
+    stdio = { stdin, nil, stderr },
+  }, function(code, signal)
+    if code == 0 then
+      vim.notify("Async Copy To Windows Clipboard Success", vim.log.levels.INFO)
+    end
+  end)
+
+  vim.uv.write(stdin, table.concat(lines, "\n"), function(err)
+    if err then
+      vim.notify("Write error: " .. err, vim.log.levels.ERROR)
+    end
+    vim.uv.close(stdin)
+  end)
+
+  vim.uv.read_start(stderr, function(err, data)
+    assert(not err, err)
+    if data then
+      vim.notify("stderr async_copy chunk: " .. data, vim.log.levels.ERROR)
+    end
+    vim.uv.close(stderr)
+  end)
+end
+
 if vim.fn.has("wsl") == 1 then
   vim.g.clipboard = {
     name = "win32clipboard",
     copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
+      ["+"] = async_copy,
+      ["*"] = async_copy,
     },
     paste = {
       ["+"] = "win32yank.exe -o --lf",
@@ -15,5 +43,6 @@ if vim.fn.has("wsl") == 1 then
     cache_enabled = 0,
   }
 end
+
 vim.opt.cursorline = false
 vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#e4e022", bg = "NONE" })
